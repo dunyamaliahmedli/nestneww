3753ebb9cf8cfa4a850bc8fa561b8e479bdab0bf
Dunyamali Ahmadli <tu7bg7w6d@aztu.local>
2022-06-01 17:59:31 +0400

    added task file

---
+++ WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication4.DAL;
+
+namespace WebApplication4.Controllers
+{
+    public class HomeController : Controller
+    {
+        private AppDbContext _context { get; }
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var sliders = _context.Sliders.ToList();
+            return View(sliders);
+        }
+    }
+}
---
+++ WebApplication4/Models/Slider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication4.Models
+{
+    public class Slider
+    {
+        public int Id { get; set; }
+        public string Title1 { get; set; }
+        public string Title2 { get; set; }
+        public string Decription { get; set; }
+        public string Image { get; set; }
+
+    }
+}
